b4c2c76b1d139066e2ec14bca644e130d47d83d7
Maxou <128489223+M-A-X-O-U@users.noreply.github.com>
2025-05-20 10:32:01 +0200

    Update header.php

--- concours_photos/views/header.php
+++ concours_photos/views/header.php
@@ -2,14 +2,14 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Appli procédures</title>
+    <title>CONCOURS PHOTO DE L’IUT DE CHÂTELLERAULT 2025</title>
     <link rel="stylesheet" type="text/css" href="css/global.css" />
 </head>
 
 <body>
 <header>
-	<a href="index.php"><img src="images/idea.png" alt="Retour vers la page d'accueil" /></a>
-	<h1>Application "Procédures"</h1>
+	<img src="images/idea.png" alt="Retour vers la page d'accueil" />
+	<h1></h1>
 	<h2>Base de données de résolution de pannes</h2>
 	<nav>
 	<ul>
