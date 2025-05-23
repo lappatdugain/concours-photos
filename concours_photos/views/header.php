8819cb66ce1ac20cc5895901d8261399885789ab
Maxou <128489223+M-A-X-O-U@users.noreply.github.com>
2025-05-23 10:19:19 +0200

    Create header.php

---
+++ concours_photos/views/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Concours Photo 2025</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+<header class="header">
+  <div class="logo">
+    <img src="logo-univ-poitiers.png" alt="Université de Poitiers">
+  </div>
+  <h1>CONCOURS PHOTO DE L’IUT DE CHÂTELLERAULT 2025</h1>
+  <div class="logo-iut">
+    <img src="logo-iut.png" alt="IUT Poitiers Niort Châtellerault">
+  </div>
+</header>
+
+
+<article>
