6ece14ec96558ad6453efdfb92099597875bd6b8
Maxou <128489223+M-A-X-O-U@users.noreply.github.com>
2025-05-23 11:24:12 +0200

    Update config.php

--- concours_photos/config/config.php
+++ concours_photos/config/config.php
@@ -1,5 +1,5 @@
 <?php
-define('HOST', 'localhost');
-define('DB', 'r209_procedures');
-define('USER', 'vincent');
-define('PASSWORD', '');
+define('HOST', '192.168.156.221');
+define('DB', 'projet2_tp2');
+define('USER', 'rlapaduh');
+define('PASSWORD', 'rt2425');
