410210f63892dae0b66e7f0a63cff664ef8dbae4
Maxou <128489223+M-A-X-O-U@users.noreply.github.com>
2025-06-04 11:01:36 +0200

    Create recherche_ldap_ctrl.php

---
+++ concours_photos/controllers/recherche_ldap_ctrl.php
@@ -0,0 +1,14 @@
+<?php
+function recherche_ldap_ctrl() {
+  define('SERVER', 'ldaps://ldapsupannappli.univ-poitiers.fr:636');
+  define('ROOT', 'ou=people,dc=univ-poitiers,dc=fr');
+    $login = $_POST['login'];
+    $pass = $_POST['pass'];
+  
+    require('models/recherche_ldap.crud.php');
+    recherche_ldap($login, $pass);
+    
+
+    require('views/accueil.php');
+    
+}
