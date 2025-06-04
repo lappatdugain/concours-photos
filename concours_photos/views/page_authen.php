98875aff77320ed27e523c78954735b412ab87c7
Maxou <128489223+M-A-X-O-U@users.noreply.github.com>
2025-06-04 09:59:40 +0200

    Create page_authen.php

---
+++ concours_photos/views/page_authen.php
@@ -0,0 +1,14 @@
+<article id="authen">
+  <form action="fichier_php" method="get">
+  <div id="login">
+    <label for="login">Login : </label>
+    <input type="email" name="login" id="login" required />
+  </div>
+  <div id="mdp">
+    <label for="mdp">Mot de Passe : </label>
+    <input type="text" name="mdp" id="mdp" required />
+  </div>
+  <div id="valider">
+    <input type="submit" value="Valider" id="bouton"/>
+  </div>
+  </form>
