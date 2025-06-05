520414619869941fcc0ff4d10e9aaed268fd6b52
Maxou <128489223+M-A-X-O-U@users.noreply.github.com>
2025-06-05 14:40:45 +0200

    Update header_authen.php

--- concours_photos/views/header_authen.php
+++ concours_photos/views/header_authen.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Concours Photo 2025</title>
-    <link rel="stylesheet" type="text/css" href="../css/global.css" />
+    <link rel="stylesheet" type="text/css" href="./css/global.css" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
@@ -14,7 +14,7 @@
   </div>
   <h1>CONCOURS PHOTO DE L’IUT DE CHÂTELLERAULT 2025</h1>
   <div class="logo-iut">
-    <img src="../images/logo-iut.png" alt="IUT Poitiers Niort Châtellerault">
+    <img src="./images/logo-iut.png" alt="IUT Poitiers Niort Châtellerault">
   </div>
 </header>
 
