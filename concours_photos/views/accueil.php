c917b101828be017948b37a09ae92ece9ea10880
itzukia <justinesgrt05@gmail.com>
2025-06-10 22:18:05 +0200

    Accueil mis à jour

--- concours_photos/views/accueil.php
+++ concours_photos/views/accueil.php
@@ -1,10 +1,15 @@
-<?php require('header.php')?>
+<?php require 'header.php'; ?>
 
-<h2>Page d'accueil</h2>
-<p><em>En cours de développement...</em></p>
-<p>Pour le moment la seule possibilité est de commencer par visualiser les familles de matériel<p>
+<link rel="stylesheet" href="../css/global.css">
 
-<?php 
-echo '<p>Ici : <a href="index.php?route=families">Les familles de matériel</a></p>';
+<div class="center-box">
+    <h2>Page d'accueil</h2>
+    <p><em>En cours de développement...</em></p>
+    <p>Pour le moment la seule possibilité est de commencer par visualiser les familles de matériel</p>
 
-require('footer.php');
+    <?php
+    echo '<p>Ici : <a href="index.php?route=families">Les familles de matériel</a></p>';
+    ?>
+</div>
+
+<?php require 'footer.php'; ?>
