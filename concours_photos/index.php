1f3e924feb2062c1b259a055f880d00497199244
Maxou <128489223+M-A-X-O-U@users.noreply.github.com>
2025-06-11 09:34:24 +0200

    Update index.php

--- concours_photos/index.php
+++ concours_photos/index.php
@@ -21,21 +21,7 @@
             require('controllers/recherche_ldap_ctrl.php');
             recherche_ldap_ctrl();
             break;
-            
-        case 'family':
-            require('controllers/family_ctrl.php');
-            family_print_ctrl();
-            break;
-            
-        case 'contact':
-            require('views/contact_view.php');
-            break;
-            
-        case 'solutions':
-            require('controllers/solution_ctrl.php');
-            solutions_list_ctrl();
-            break;
-            
+                    
         case 'vote':
             require('views/vote_view.php');
 #            add_equipment_ctrl();
